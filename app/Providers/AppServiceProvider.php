b9807caf7d73697d60dc95fa9bc0543f24dcd494
Tresnarihermana <Tresnarihermana@gmail.com>
2025-07-25 17:53:50 +0700

    NoCaptcha atau apalah

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Anhskohbo\NoCaptcha\NoCaptcha;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+          Validator::extend('captcha', function ($attribute, $value, $parameters, $validator) {
+        return app(NoCaptcha::class)->verifyResponse($value);
+    });
     }
 }
