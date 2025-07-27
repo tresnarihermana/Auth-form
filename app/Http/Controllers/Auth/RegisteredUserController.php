f6dbb91d3c90ae36f2817d1f8814d27ea7dbe581
Tresnarihermana <Tresnarihermana@gmail.com>
2025-07-27 11:06:07 +0700

    inisialisai recaptcha

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Log;
 use App\Models\User;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Validation\Rules\Password;
@@ -36,6 +37,7 @@ class RegisteredUserController extends Controller
             'username' => 'required|string|max:255|regex:/^[a-zA-Z0-9_]+$/|unique:'.User::class,
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults(), Password::min(8)->numbers()->symbols()->max(255)->mixedCase(),'regex:/^[A-Za-z\d\W_]+$/'],
+            'g-recaptcha-response' => 'required',
         ], [
             'name.required' => 'Name is required.',
             'username.unique' => 'Username is already taken.',
@@ -45,6 +47,20 @@ class RegisteredUserController extends Controller
             'password.regex'=> 'Password can olny contain letters, numbers, and underscores.',
             'password.confirmed' => 'Passwords do not match'
         ]);
+        // Verifikasi ke Google reCAPTCHA
+        // dd($request->all()); -> ini buat debug ges
+        $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => config('services.recaptcha.secret'),
+            'response' => $request->input('g-recaptcha-response'),
+            'remoteip' => $request->ip(),
+        ]);
+        if (!($response->json('success'))) {
+            return back()->withErrors([
+                'g-recaptcha-response' => 'Verifikasi captcha gagal. Silakan coba lagi.',
+            ])->withInput();
+        }
+        
+
 
         $user = User::create([
             'name' => $request->name,
