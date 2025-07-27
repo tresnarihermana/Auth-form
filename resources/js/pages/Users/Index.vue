075585e245fbc7540f13b7a1a9aa5ed3932f47b6
Tresnarihermana <Tresnarihermana@gmail.com>
2025-07-27 18:39:29 +0700

    tambah halaman kelola users

---
+++ resources/js/pages/Users/Index.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head, useForm } from '@inertiajs/vue3';
+import Button from 'primevue/button';
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Users',
+        href: '/users',
+    },
+];
+defineProps({
+    users: Array
+});
+
+
+
+</script>
+
+<template>
+    <Head title="Users" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+<!-- component -->
+<div class="text-gray-900 bg-gray-200">
+    <div class="p-4 flex">
+        <h1 class="text-3xl">
+            Users
+        </h1>
+    </div>
+    <div class="px-4">
+    <Button label="Add User" as="a" :href="route('users.create')"  icon="pi pi-plus" icon-pos="left" />  
+</div>
+    <div class="px-3 py-4 flex justify-center">
+        
+        <table class="w-full text-md bg-white shadow-md rounded mb-4">
+            <tbody>
+                <tr class="border-b">
+                    <th class="text-left p-3 px-5">Name</th>
+                    <th class="text-left p-3 px-5">Username</th>
+                    <th class="text-left p-3 px-5">Email</th>
+                    <th class="text-left p-3 px-5">Role</th>
+                    <th></th>
+                </tr>
+                <tr  v-for="user in users" class="border-b hover:bg-orange-100 bg-gray-100">
+                    <td class="p-3 px-5"><input type="text" v-model="user.name"class="bg-transparent"></td>
+                    <td class="p-3 px-5"><input type="text" v-model="user.username"class="bg-transparent"></td>
+                    <td class="p-3 px-5"><input type="text" v-model="user.email" class="bg-transparent"></td>
+                    <td class="p-3 px-5"><input type="text" v-model="user.email" class="bg-transparent"></td>
+                    <td class="p-3 px-5 flex justify-end"><button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button></td>
+                    <td class="p-3 px-5 flex justify-end"><button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+    </AppLayout>
+</template>
