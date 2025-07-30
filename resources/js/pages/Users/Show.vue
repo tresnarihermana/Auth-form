6cf6ff54767022ea5c4334a2803c2c8422a55bad
Tresnarihermana <Tresnarihermana@gmail.com>
2025-07-30 17:42:36 +0700

    refactoring + add delete user button

--- resources/js/pages/Users/Show.vue
+++ resources/js/pages/Users/Show.vue
@@ -10,10 +10,11 @@ import { reactive, watch, ref, onMounted } from 'vue';
 import Swal from 'sweetalert2';
 import InputText from 'primevue/inputtext'
 import Password from 'primevue/password';
-import { Save } from 'lucide-vue-next';
+import { TrashIcon, PencilIcon } from 'lucide-vue-next';
 import Checkbox from 'primevue/checkbox';
 import { useInitials } from '@/composables/useInitials';
-
+import { can } from '@/lib/can';
+import { router } from '@inertiajs/vue3';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'User Edit',
@@ -73,17 +74,51 @@ function validatePassword() {
 }
 watch(() => form.password, validatePassword)
 const { getInitials } = useInitials();
+function deleteUser(id) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            router.delete(route('users.destroy', id), {
+                onSuccess: () => {
+                    Swal.fire({
+                        title: 'Deleted!',
+                        text: 'This user has been deleted.',
+                        icon: 'success',
+                        timer: 1000,
+                        timerProgressBar: true,
+                    });
+                },
+                onError: () => {
+                    Swal.fire({
+                        title: 'Failed!',
+                        text: 'Something went wrong. The role was not deleted.',
+                        icon: 'error',
+                    });
+                }
+            });
+        }
+    });
+}
 </script>
+
+
 
 <template>
 
-    <Head title="Users Edit" />
+    <Head title="User Info" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <!-- component -->
         <div class="text-gray-900 bg-gray-200">
             <div class="p-4 flex">
                 <h1 class="text-3xl">
-                    Edit Existed User
+                    User Info
                 </h1>
             </div>
 
@@ -97,18 +132,19 @@ const { getInitials } = useInitials();
             <!-- component -->
             <div class="flex items-center h-screen w-full justify-center">
 
-                <div class="max-w-xs">
+                <div class="max-w-lg">
                     <div class="bg-white shadow-xl rounded-lg py-3">
                         <div class="photo-wrapper p-2">
-                            <img class="w-32 h-32 rounded-full mx-auto"
-                                :src="props.user.avatar ? `/storage/` + props.user.avatar : 'https://ui-avatars.com/api/?name=' + getInitials(props.user.username) + '&background=random'"
-                                :alt="props.user.name + `avatar`"
-                                alt="John Doe">
+                    <img class="w-full h-56 object-cover object-center"
+                        :src="props.user.avatar ? `/storage/` + props.user.avatar : 'https://ui-avatars.com/api/?name=' + getInitials(props.user.username) + '&background=random'"
+                        :alt="props.user.name + `avatar`" alt="John Doe">
+          
                         </div>
                         <div class="p-2">
-                            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">{{ props.user.username }}</h3>
+                            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">{{ props.user.name }}
+                            </h3>
                             <div class="text-center text-blue-400 text-xs font-semibold">
-                                {{ props.userRoles[0] }}
+                                {{ props.user.username }}
                             </div>
                             <table class="text-xs my-3">
                                 <tbody>
@@ -126,23 +162,46 @@ const { getInitials } = useInitials();
                                     </tr>
                                     <tr>
                                         <td class="px-2 py-2 text-gray-500 font-semibold">Email Verified At</td>
-                                        <td class="px-2 py-2">{{props.user.email_verified_at}}</td>
+                                        <td class="px-2 py-2" v-if="props.user.email_verified_at">
+                                            {{ props.user.email_verified_at }}</td>
+                                        <td class="px-2 py-2 text-red-500" v-else>{{ 'Not Verified' }}</td>
                                     </tr>
                                     <tr>
                                         <td class="px-2 py-2 text-gray-500 font-semibold">Created at</td>
-                                        <td class="px-2 py-2">{{ props.user.created_at}}</td>
+                                        <td class="px-2 py-2">{{ props.user.created_at }}</td>
                                     </tr>
                                     <tr>
                                         <td class="px-2 py-2 text-gray-500 font-semibold">Updated at</td>
                                         <td class="px-2 py-2">{{ props.user.updated_at }}</td>
                                     </tr>
- 
+                                    <tr>
+                                        <td class="px-2 py-2 text-gray-500 font-semibold">Role</td>
+                                        <td class="px-2 py-2 text-blue-500 font-semibold">{{ String(props.userRoles) }}
+                                        </td>
+                                    </tr>
+
                                 </tbody>
                             </table>
 
                             <div class="text-center my-3">
-                                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
-                                    href="#">View Profile</a>
+                                <table >
+                                    <tbody>
+                                        <tr>
+                                            <td class="px-2 py-2">
+                                                <Button  v-if="can('users.edit')" as="a" :href="route('users.edit', props.user.id)" size="small" severity="info">
+                                                    <PencilIcon />
+                                                    Edit
+                                                </Button>
+                                            </td>
+                                            <td class="px-2 py-2">
+                                                <Button v-if="can('users.delete')" @click="deleteUser(props.user.id)" size="small" severity="danger">
+                                                    <TrashIcon />
+                                                    Delete
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
 
                         </div>
