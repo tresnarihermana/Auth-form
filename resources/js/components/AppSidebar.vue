89b1c7640134fea204fc785c2d7b8123f8fdcc5d
Tresnarihermana <Tresnarihermana@gmail.com>
2025-07-30 17:35:06 +0700

    tambah link permission di sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, Users, UserRoundCog } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Users, UserRoundCog, ShieldCheck } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -23,6 +23,11 @@ const mainNavItems: NavItem[] = [
         title: 'Roles',
         href: '/roles',
         icon: UserRoundCog,
+    },
+    {
+        title: 'Permissions',
+        href: '/permissions',
+        icon: ShieldCheck,
     },
 ];
 
